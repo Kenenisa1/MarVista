72850185ba52dc35d0176581a3d44ae16dba6180
kenenisa1 <kenenisamiesob@gmail.com>
2025-12-09 22:35:36 +0300

    Feat(server): Add userRoutes to server.js file

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
-import { mongoose } from "mongoose"; 
+import productRoutes from "./routes/product.route.js"; 
+import userRoutes from './routes/user.route.js'
 
 dotenv.config();
 
@@ -10,69 +10,12 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api/products", async(req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json({ success: true, data: products})
-  }
-  catch (err) {
-    res
-    .status(400).json({ success: false, message: "Unable to get the products"});
-  }
-})
-app.post("/api/products", async (req, res) => {
-  const product = req.body;
+app.use("/api/products", productRoutes);
+app.use('/api/users', userRoutes)
 
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide all required fields" });
-  }
-  const newProduct = new Product(product);
+const PORT = process.env.PORT || 5000;
 
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const DeleteProduct=  await Product.findByIdAndDelete(id);
-
-    if(!DeleteProduct)
-    {
-        res.status(400).json({success: false, message: "Product not found"})
-    }
-    res.status(200).json({ success: true, message: "Product deleted successfully" });
-  } catch (err) {
-    console.log(`${err}`);
-  }
-});
-
-app.put("/api/products/:id", async (req, res) => {
-  const {id} = req.params;
-  
-  const product = req.body;
-
-  if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(400).json({success: false, message: "Invalid product ID"});
-  }
-
-  try {
-    const updatedProduct = await Product.findByIdAndUpdate(id, product,{new: true});
-    res.status(200).json({success: true, data: updatedProduct})
-  }
-
-  catch  {
-    res.status(500).json({success: false, message: "Server Error"})
-  }
-})
-
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on port 5000");
+  console.log(`Server is running on localhost:${PORT}`);
 });
