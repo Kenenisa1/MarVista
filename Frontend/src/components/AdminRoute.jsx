0f31b4524b4a30fcabaf54d640e99e319e84c328
kenenisa1 <kenenisamiesob@gmail.com>
2025-12-30 22:57:43 +0300

    feat(components) : Add AdminRoute component for admin

---
+++ Frontend/src/components/AdminRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from 'react-router-dom';
+import { useUserStore } from '../Store/user';
+
+export const AdminRoute = ({ children }) => {
+    const { user } = useUserStore();
+    
+    if (!user || user.isAdmin !== true) {
+        return <Navigate to="/" replace />;
+    }
+    
+    return children;
+};
