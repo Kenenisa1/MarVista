54e2869e4b2b762cc39ed7c380c9b6d4681e71b7
kenenisa1 <kenenisamiesob@gmail.com>
2026-01-14 14:39:09 +0300

    feat(src) : Add routeConfig file for routes

---
+++ Frontend/src/routeConfig.js
@@ -0,0 +1,27 @@
+// src/routeConfig.js - Route configuration
+export const routes = [
+  { path: '/', element: 'HomePage' },
+  { path: '/about', element: 'About' },
+  { path: '/contact', element: 'Contact' },
+  { path: '/home', element: 'HomePage' },
+  { path: '/Product/:id', element: 'ProductDetail' },
+  { 
+    path: '/create', 
+    element: 'CreatePage',
+    protected: true,
+    admin: true 
+  },
+  { 
+    path: '/products/:id/edit', 
+    element: 'UpdateProductPage',
+    protected: true,
+    admin: true 
+  },
+  { path: '/signin', element: 'SignIn' },
+  { path: '/signup', element: 'SignUp' },
+  { 
+    path: '/products', 
+    element: 'Products',
+    protected: true 
+  },
+];
