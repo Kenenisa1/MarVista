ddff7c555bfaf4cad688f2600c31a826877163db
kenenisa1 <kenenisamiesob@gmail.com>
2026-01-21 23:17:19 +0300

    feat(imports) : Add file imports

---
+++ Frontend/src/imports/index.js
@@ -0,0 +1,15 @@
+// src/imports/index.js - Centralized imports
+export { default as Navbar } from '../components/Navbar';
+export { default as Footer } from '../components/Footer';
+export { default as Products } from '../components/Products';
+export { default as AdminRoute } from '../components/AdminRoute';
+export { default as ProtectedRoute } from '../components/ProtectedRoute';
+
+export { default as HomePage } from '../pages/HomePage';
+export { default as CreatePage } from '../pages/CreatePage';
+export { default as SignIn } from '../pages/SignIn';
+export { default as SignUp } from '../pages/SignUp';
+export { default as UpdateProductPage } from '../pages/UpdateProductPage';
+export { default as ProductDetail } from '../pages/ProductDetail';
+export { default as About } from '../pages/About';
+export { default as Contact } from '../pages/Contact';
